a5a2c5211b6f5778be04451bae11f56f75526c2d
xuting1108 <xuting1108@hotmail.com>
2021-04-07 18:10:06 -0300

    Add user profile feed API

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractBaseUser,BaseUserManager, PermissionsMixin
 
 
@@ -54,3 +55,15 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         """Return string representation of a user profile"""
         return self.email
+
+class ProfileFeedItem(models.Model):
+    """Profile status update"""
+
+    user_profile = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        """Return the model as a string"""
+        return self.status_text
+    
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,11 @@
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework import filters, status, viewsets
 from rest_framework.authentication import TokenAuthentication
-
-from rest_framework.settings import api_settings
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.settings import api_settings
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 from profiles_api import models
 from profiles_api import serializers
@@ -115,3 +116,15 @@ class UserLoginAPIView(ObtainAuthToken):
     
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
     
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnStatus, IsAuthenticated)
+    
+    serializer_class = serializers.ProfileFeedItemSerializer
+    
+    queryset = models.ProfileFeedItem.objects.all()
+    
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
